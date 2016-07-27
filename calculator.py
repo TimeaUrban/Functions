c5bb1cf64693e9af2bfe68b9f78aa5286a6063c8
Timea <timea@gmail.com>
2016-07-26 21:10:42 -0700

    Added new calculator file

---
+++ calculator.py
@@ -0,0 +1,12 @@
+def add (num1, num2):
+	return num1 + num2
+
+def subtract(num1, num2):
+	return num2 - num1
+
+def multiply (num1, num2):
+	return num1 * num2
+
+#print add (4, 5)
+# print subtract(4,5)
+print multiply (3,4)
