18c56080503c8b8306462f958ec0d004b7ae9ff7
Timea Urban <timellya@gmail.com>
2016-07-28 10:27:07 -0700

    Calculator homework done

--- calculator.py
+++ calculator.py
@@ -1,12 +1,55 @@
-def add (num1, num2):
+def add(num1,num2):
 	return num1 + num2
 
-def subtract(num1, num2):
-	return num2 - num1
+def subtract(num1,num2):
+	return num1 - num2
 
-def multiply (num1, num2):
+def multiply(num1,num2):
 	return num1 * num2
 
-#print add (4, 5)
-# print subtract(4,5)
-print multiply (3,4)
+def divide(num1,num2):
+	return num1 / num2
+
+def modulo(num1,num2):
+	return num1 % num2
+
+def power(base,exponent):
+	return base ** exponent
+
+def square(num):
+	return num ** 2
+
+print add(4,5)
+print subtract(10,5)
+print multiply(6,6)
+print divide(10,4)
+print modulo(10,5)
+print power(12,2)
+print square(12)
+
+
+# num1 = 4 + 5
+# num2 = 9 - 6
+# print num1 + num2
+
+# print 12/2 - 60
+
+# print 1 + 2 + 3
+
+# num1 = 1 + 2
+# print num1 * 2
+
+# num1 = 3 % 4
+# num2 = 9 * 9
+# print num1 / num2
+
+# num1 = 3 + 8
+# print 7 * num1
+
+# num1 = 1 + 2
+# num2 = 4 + 5
+# print num1 - 3 * num2
+
+# num1 = 2 + 3
+# print 3 ** num1
+
